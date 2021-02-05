55a0f46e534cf503213ae34c03c8ed114aab4aa6
25-Naufal <naufal_rafif_29rpl@student.smktelkom-mlg.sch.id>
2021-02-05 09:57:36 +0700

    hore berhasil

---
+++ src/kelulusan/siswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelulusan;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class siswa {
+ // mendifinisikan atribut
+ String nama;
+ int nilai;
+
+ //method cetak nilai
+ public void cetaknilai(){
+ System.out.println("Nilai Produktif RPL 5 "+nama+" adalah =" +nilai);
+ }
+
+ //method pengecekan
+ public boolean lulus(){
+ return nilai>=75;
+ }
+ public boolean tidaklulus(){
+ return nilai<74;
+ }
+}
+
